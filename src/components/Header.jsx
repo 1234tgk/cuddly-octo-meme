50efa8fa97ea27787c1cc57a4a7c90d59f59bb06
1234tgk <1234tgk@gmail.com>
2023-08-11 13:38:27 -0400

    add clock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,35 @@
 import { Link } from "react-router-dom";
+import Clock from "./Clock";
 import "./Header.css";
 
 const Header = (props) => {
   return (
-    <header className="header">
-      <nav>
-        <a href="https://github.com/1234tgk">
-          <img
-            alt="GitHub Icon"
-            src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg"
-            className="icon"
-          />
-        </a>
-        <Link to="/">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
-            alt="Home"
-            className="icon"
-          />
-        </Link>
-      </nav>
+    <div>
+      <header className="header">
+        <nav>
+          <a href="https://github.com/1234tgk">
+            <img
+              alt="GitHub Icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg"
+              className="icon"
+            />
+          </a>
+          <Link to="/">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
+              alt="Home"
+              className="icon"
+            />
+          </Link>
+        </nav>
 
-      <Link className="name" to="/profile">
-        <p>{props.name}</p>
-      </Link>
-    </header>
+        <Link className="name" to="/profile">
+          <p>{props.name}</p>
+        </Link>
+      </header>
+
+      <Clock />
+    </div>
   );
 };
 
